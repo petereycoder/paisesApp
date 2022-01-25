0dc0a9ca41be6bbf8005471b1bb8e14084d5aeb2
petereycoder <fp.pipe.reyes@gmail.com>
2022-01-25 13:22:08 -0600

    Paises App parte uno

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent implements OnInit {
+
+  pais!: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisServices: PaisService
+    ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.paisServices.getPaisPorAlpha(id)),
+      tap(console.log)
+    )
+    .subscribe(pais => this.pais = pais[0]);
+    //this.activatedRoute.params
+      //.subscribe( ({id}) =>{
+        //console.log(id);
+        
+        //this.paisServices.getPaisPorAlpha(id)
+        //.subscribe(pais =>{
+          //console.log(pais);
+        //});
+      //});
+  }
+
+
+}
